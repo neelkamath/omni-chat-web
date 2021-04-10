5630eec186833957989d0cd9189d9783e2252a01
neelkamath <neelkamathonline@gmail.com>
2021-04-10 10:15:37 +0530

    Remove unused dependency

--- src/components/chat-page/account-editor/AccountEditor.tsx
+++ src/components/chat-page/account-editor/AccountEditor.tsx
@@ -1,28 +1,14 @@
-import React, { ReactElement, useState } from 'react';
-import { DeleteOutlined, UploadOutlined } from '@ant-design/icons';
-import { Storage } from '../../../Storage';
-import logOut from '../../../logOut';
-import { Button, Divider, message, Row, Space, Spin, Typography, Upload } from 'antd';
-import { ShowUploadListInterface } from 'antd/lib/upload/interface';
-import OriginalProfilePic from '../OriginalProfilePic';
-import { deleteProfilePic, InvalidPicError, NonexistentUserIdError, patchProfilePic } from '@neelkamath/omni-chat';
-import { useSelector } from 'react-redux';
-import { RootState, useThunkDispatch } from '../../../store/store';
-import { PicsSlice } from '../../../store/slices/PicsSlice';
-import { UploadRequestOption as RcCustomRequestOptions } from 'rc-upload/lib/interface';
+import React, { ReactElement } from 'react';
+import { Divider, Row } from 'antd';
 import UpdateAccountSection from './UpdateAccountSection';
 import UpdatePasswordForm from './UpdatePasswordForm';
 import DeleteAccountSection from './DeleteAccountSection';
-import { httpApiConfig, operateGraphQlApi, operateRestApi } from '../../../api';
+import ProfilePicEditor from './ProfilePicEditor';
 
 export default function AccountEditor(): ReactElement {
   return (
     <Row style={{ padding: 16 }}>
-      <Space direction='vertical'>
-        <ProfilePic />
-        <NewProfilePicButton />
-        <DeleteProfilePicButton />
-      </Space>
+      <ProfilePicEditor />
       <Divider />
       <UpdateAccountSection />
       <Divider />
@@ -33,48 +19,3 @@ export default function AccountEditor(): ReactElement {
   );
 }
 
-function ProfilePic(): ReactElement {
-  const userId = Storage.readUserId()!;
-  const url = useSelector((state: RootState) => PicsSlice.selectPic(state, 'PROFILE_PIC', userId, 'ORIGINAL'));
-  const error = useSelector((state: RootState) => PicsSlice.selectError(state, 'PROFILE_PIC', userId));
-  if (error instanceof NonexistentUserIdError) logOut();
-  useThunkDispatch(PicsSlice.fetchPic({ id: userId, type: 'PROFILE_PIC' }));
-  if (url === undefined) return <Spin size='small' />;
-  else if (url === null) return <Typography.Text>No profile picture set.</Typography.Text>;
-  else return <OriginalProfilePic url={url} />;
-}
-
-function NewProfilePicButton(): ReactElement {
-  const [showUploadList, setShowUploadList] = useState<ShowUploadListInterface | boolean>({ showRemoveIcon: false });
-  const customRequest = async ({ file }: RcCustomRequestOptions) => {
-    await operatePatchProfilePic(file as File);
-    setShowUploadList(false);
-  };
-  return (
-    <Upload showUploadList={showUploadList} customRequest={customRequest} accept='image/png,image/jpeg'>
-      <Button icon={<UploadOutlined />}>New Profile Picture</Button>
-    </Upload>
-  );
-}
-
-async function operatePatchProfilePic(file: File): Promise<void> {
-  try {
-    await operateRestApi(() => patchProfilePic(httpApiConfig, Storage.readAccessToken()!, file));
-    message.success('Profile picture updated.', 3);
-  } catch (error) {
-    if (error instanceof InvalidPicError) message.error('The picture mustn\'t exceed 5 MB.', 5);
-    else throw error;
-  }
-}
-
-function DeleteProfilePicButton(): ReactElement {
-  const onClick = async () => {
-    const result = await operateGraphQlApi(() => deleteProfilePic(httpApiConfig, Storage.readAccessToken()!));
-    if (result !== undefined) message.success('Profile picture deleted.');
-  };
-  return (
-    <Button danger icon={<DeleteOutlined />} onClick={onClick}>
-      Delete Profile Picture
-    </Button>
-  );
-}
---
+++ src/components/chat-page/account-editor/ProfilePicEditor.tsx
@@ -0,0 +1,69 @@
+import React, { ReactElement, useState } from 'react';
+import { Button, message, Space, Spin, Typography, Upload } from 'antd';
+import { Storage } from '../../../Storage';
+import { useSelector } from 'react-redux';
+import { RootState, useThunkDispatch } from '../../../store/store';
+import { PicsSlice } from '../../../store/slices/PicsSlice';
+import { deleteProfilePic, InvalidPicError, NonexistentUserIdError, patchProfilePic } from '@neelkamath/omni-chat';
+import logOut from '../../../logOut';
+import OriginalProfilePic from '../OriginalProfilePic';
+import { ShowUploadListInterface } from 'antd/lib/upload/interface';
+import { UploadRequestOption as RcCustomRequestOptions } from 'rc-upload/lib/interface';
+import { DeleteOutlined, UploadOutlined } from '@ant-design/icons';
+import { httpApiConfig, operateGraphQlApi, operateRestApi } from '../../../api';
+
+export default function ProfilePicEditor(): ReactElement {
+  return (
+    <Space direction='vertical'>
+      <ProfilePic />
+      <NewProfilePicButton />
+      <DeleteProfilePicButton />
+    </Space>
+  );
+}
+
+function ProfilePic(): ReactElement {
+  const userId = Storage.readUserId()!;
+  const url = useSelector((state: RootState) => PicsSlice.selectPic(state, 'PROFILE_PIC', userId, 'ORIGINAL'));
+  const error = useSelector((state: RootState) => PicsSlice.selectError(state, 'PROFILE_PIC', userId));
+  if (error instanceof NonexistentUserIdError) logOut();
+  useThunkDispatch(PicsSlice.fetchPic({ id: userId, type: 'PROFILE_PIC' }));
+  if (url === undefined) return <Spin size='small' />;
+  else if (url === null) return <Typography.Text>No profile picture set.</Typography.Text>;
+  else return <OriginalProfilePic url={url} />;
+}
+
+function NewProfilePicButton(): ReactElement {
+  const [showUploadList, setShowUploadList] = useState<ShowUploadListInterface | boolean>({ showRemoveIcon: false });
+  const customRequest = async ({ file }: RcCustomRequestOptions) => {
+    await operatePatchProfilePic(file as File);
+    setShowUploadList(false);
+  };
+  return (
+    <Upload showUploadList={showUploadList} customRequest={customRequest} accept='image/png,image/jpeg'>
+      <Button icon={<UploadOutlined />}>New Profile Picture</Button>
+    </Upload>
+  );
+}
+
+async function operatePatchProfilePic(file: File): Promise<void> {
+  try {
+    await operateRestApi(() => patchProfilePic(httpApiConfig, Storage.readAccessToken()!, file));
+    message.success('Profile picture updated.', 3);
+  } catch (error) {
+    if (error instanceof InvalidPicError) message.error('The picture mustn\'t exceed 5 MB.', 5);
+    else throw error;
+  }
+}
+
+function DeleteProfilePicButton(): ReactElement {
+  const onClick = async () => {
+    const result = await operateGraphQlApi(() => deleteProfilePic(httpApiConfig, Storage.readAccessToken()!));
+    if (result !== undefined) message.success('Profile picture deleted.');
+  };
+  return (
+    <Button danger icon={<DeleteOutlined />} onClick={onClick}>
+      Delete Profile Picture
+    </Button>
+  );
+}
