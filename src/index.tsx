987028233d4a3f5522555eee2dcf725ee7a373a6
neelkamath <neelkamathonline@gmail.com>
2021-07-07 06:15:21 +0530

    Remove stale "TODO" comments

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import setOnline from './setOnline';
 
-// FIXME: Viewing a public chat the user isn't in doesn't yield updates.
-// FIXME: When a user gets added to a public chat, the message creator still says they aren't added if they're already viewing the chat.
-
 ReactDOM.render(<App />, document.querySelector('#root'));
 /*
 Make the user offline if they manually navigate away from the chat page. We must use the <beforeunload> event because
